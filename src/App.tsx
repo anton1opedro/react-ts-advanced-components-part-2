b04f3885bfc35435677fd66ca2476eabc2e76ae1
apsousa <apsousa@keep.pt>
2025-07-28 16:49:54 +0100

    Implement starting project

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,17 @@
-export default function App() {
-  return <h1>Let's get started!</h1>;
+import AddTimer from "./components/AddTimer.tsx";
+import Header from "./components/Header.tsx";
+import Timers from "./components/Timers.tsx";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <AddTimer />
+        <Timers />
+      </main>
+    </>
+  );
 }
+
+export default App;
