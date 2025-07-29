024c5ac777d1809f4d76fc84b650e428f1a9af4c
apsousa <apsousa@keep.pt>
2025-07-29 16:02:43 +0100

    Enhance Timer component with countdown functionality and progress display

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,11 +1,49 @@
 import Container from "./UI/Container.tsx";
-import type { Timer as TimerProps } from "../store/timers-context.tsx";
+import {
+  type Timer as TimerProps,
+  useTimersContext,
+} from "../store/timers-context.tsx";
+import { useEffect, useRef, useState } from "react";
 
 export default function Timer(props: TimerProps) {
+  const timersContext = useTimersContext();
+  const interval = useRef<number | null>(null);
+  const [remainingTime, setRemainingTime] = useState(props.duration * 1000);
+  const formatedTime = (remainingTime / 1000).toFixed(2);
+
+  if (remainingTime <= 0 && interval.current) {
+    clearInterval(interval.current);
+  }
+
+  useEffect(() => {
+    let timer: number;
+    if (timersContext.isRunning) {
+      timer = window.setInterval(function () {
+        setRemainingTime((prevTime) => {
+          if (prevTime <= 0) {
+            return prevTime;
+          } else {
+            return prevTime - 50;
+          }
+        });
+      }, 50);
+      interval.current = timer;
+    } else if (interval.current) {
+      clearInterval(interval.current);
+    }
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, [timersContext.isRunning]);
+
   return (
     <Container as="article">
       <h2>{props.name}</h2>
-      <p>{props.duration}</p>
+      <p>
+        <progress max={props.duration * 1000} value={remainingTime} />
+      </p>
+      <p>{formatedTime}</p>
     </Container>
   );
 }
